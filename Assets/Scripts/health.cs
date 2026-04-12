90dd76fa74dcade094a0ee44bebca8de3c16294b
Joshua Isbey <jisbey@ewu.edu>
2026-04-12 10:48:10 -0700

    put assets back in

---
+++ Assets/Scripts/health.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class Health : MonoBehaviour
+{
+    public int curHealth = 0;
+    public int maxHealth = 100;
+    private InputAction jumpAction;
+    public HealthBar healthBar;
+    void Awake()
+    {
+        jumpAction = new InputAction(type: InputActionType.Button, binding: "<Keyboard>/space");
+        jumpAction.Enable();
+    }
+
+    void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        if (jumpAction.WasPressedThisFrame())
+        {
+            DamagePlayer(10);
+        }
+    }
+
+    public void DamagePlayer( int damage )
+    {
+        curHealth -= damage;
+
+        healthBar.SetHealth( curHealth );
+    }
+}
+
+
+
+
